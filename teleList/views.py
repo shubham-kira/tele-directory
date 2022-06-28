e1e7e0c1e47354d0e1c85e880c0eaa7ec6c7625d
Shubham <shubham190shubham@gmail.com>
2022-06-28 20:53:54 +0530

    Display master list on web page

--- teleList/views.py
+++ teleList/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import TeleItem
+
 
 def master_list(request):
-    return render(request, 'teleList/master_list.html', {})
+    master_list_items = TeleItem.objects.all()
+    return render(request, 'teleList/master_list.html', {'master_list_items': master_list_items})
